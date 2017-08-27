64d035887ec41f4ed6322cdcea0b7486bdd73107
MelaniaNitu <melania.nitu@yahoo.com>
2017-08-27 16:55:39 +0300

    LA tool- modified GUI -work in progress

--- src/main/java/com/policat/LA/configs/WebMvcConfig.java
+++ src/main/java/com/policat/LA/configs/WebMvcConfig.java
@@ -15,6 +15,13 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/about").setViewName("about-page");
         registry.addViewController("/results").setViewName("results");
         registry.addViewController("/contact").setViewName("contact");
-        registry.addViewController("/tutorials").setViewName("tutorials");
+
+        registry.addViewController("/descriptive").setViewName("descriptive");
+        registry.addViewController("/diagnostic").setViewName("diagnostic");
+        registry.addViewController("/prescriptive").setViewName("prescriptive");
+        registry.addViewController("/predictive").setViewName("predictive");
+        registry.addViewController("/summary").setViewName("summary");
+        registry.addViewController("/data_analytics").setViewName("data_analytics");
+
     }
 }
