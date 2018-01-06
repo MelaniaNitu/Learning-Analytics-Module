63263fbfad811577e932bfa041514988030c0a49
Gabriel Alecu <gc.alecu@gmail.com>
2018-01-06 23:40:52 +0200

    Fixed issues when there are no quiz results available per user

--- src/main/java/com/policat/LA/dtos/DataPointLineDTO.java
+++ src/main/java/com/policat/LA/dtos/DataPointLineDTO.java
@@ -3,7 +3,6 @@ package com.policat.LA.dtos;
 public class DataPointLineDTO {
     private Integer x;
     private double y;
-    private String lineDashType;
 
     public DataPointLineDTO(Integer x, double y) {
         this.x = x;
@@ -13,7 +12,6 @@ public class DataPointLineDTO {
     public DataPointLineDTO(Integer x, double y, String lineDashType) {
         this.x = x;
         this.y = y;
-        this.lineDashType = lineDashType;
     }
 
     public Integer getX() {
@@ -30,13 +28,5 @@ public class DataPointLineDTO {
 
     public void setY(double y) {
         this.y = y;
-    }
-
-    public String getLineDashType() {
-        return lineDashType;
-    }
-
-    public void setLineDashType(String lineDashType) {
-        this.lineDashType = lineDashType;
     }
 }
